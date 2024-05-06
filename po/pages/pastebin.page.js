23bcf8db237929e73b5d44af8ffd0c735da1ca0d
Mateusz_Maj <mateusz.maj.krakow@gmail.com>
2024-05-06 13:25:55 +0200

    implement 1st test case on pastebin.com

--- po/pages/pastebin.page.js
+++ po/pages/pastebin.page.js
@@ -1,27 +1,19 @@
-import Page from './Page';
+import Page from './Page.js';
 
-export default class PasteBinPage extends Page {
+export default new class PasteBinPage extends Page {
+    get newPasteInputField() { return $('textarea#postform-text') }
+    get pasteExpirationSelectField() { return $('span#select2-postform-expiration-container') }
+    get pasteNameInputField () { return $('input#postform-name') }
+    get createNewPasteButton () { return $('button.btn[type="submit"]')}
+
     // open the tested page
-    async open () {
+    async open() {
         await super.open('https://pastebin.com');
     }
 
-    // set 'New Paste'
-    async setNewPaste (input) {
-        // find 'New Paste' input field & set its value
-        await $('textarea#postform-text').setValue(input);
-    }
-
     // set 'Paste Expiration'
-    async setExpiration (period) {
-        // in case the previous selector fails: 'span#select2-postform-expiration-container'
-        await $('select#postform-expiration').click();
+    async setPasteExpiration(period) {
+        await this.pasteExpirationSelectField.click();
         await $(`//li[text()='${period}']`).click();
-    }
-
-    // set Paste Name / Title
-    async setPasteName (input) {
-        // find 'Paste Name / Title' input field & set its value
-        await $('input#postform-name').setValue(input);
     }
 };
--- test/specs/pastebin.test.js
+++ test/specs/pastebin.test.js
@@ -1,11 +1,34 @@
-import PasteBinPage from '../../po/pages/pastebin.page';
+import PasteBinPage from '../../po/pages/pastebin.page.js';
 
 describe('https://pastebin.com', () => {
-    
-    it('creates "New Paste" by following the script', async () => {
-        await PasteBinPage.open();
-        const selectFieldCheck = await $('span#select2-postform-expiration-container').isExisting();
+    let page;
+    beforeEach(async () => {
+        PasteBinPage.open();
 
-        expect(selectFieldCheck).toEqual(true);
+        // // add cookies to prevent a pop-up window
+        // await browser.addCookie({name: '_cc_dc', value: '1'});
+        // await browser.addCookie({name: '_cc_id', value: '4a03f9d0af1def51d660c035cd475b46'});
+
+        // // handle cookies pop-up window
+        // await browser.pause(2000);
+        // const cookiesPopupIsDisplayed = await $('div#qc-cmp-ui').isDisplayed();
+        // if (cookiesPopupIsDisplayed) {
+        //     // Find and click the agree button
+        //     await $('button.css-47sehv').click();
+        // }
+
+    });
+
+    // it('displays a cookies pop-up window', async () => {
+    //     const cookiesPopup = await $('div#qc-cmp-ui');
+    //     expect(cookiesPopup).toBeDisplayed();
+    // });
+
+    it('creates "New Paste" by following the script', async () => {
+        await PasteBinPage.newPasteInputField.click();
+        await PasteBinPage.newPasteInputField.setValue('Hello from WebDriver');
+        await PasteBinPage.setPasteExpiration('10 Minutes');
+        await PasteBinPage.pasteNameInputField.setValue('helloweb');
+        await PasteBinPage.createNewPasteButton.click();
     });
 });
