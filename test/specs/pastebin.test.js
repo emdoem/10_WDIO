ead908094a272d2b5d221ecd5d89e0b1ad03c277
Mateusz_Maj <mateusz.maj.krakow@gmail.com>
2024-05-06 13:27:17 +0200

    cleanup unnecessary comments

--- test/specs/pastebin.test.js
+++ test/specs/pastebin.test.js
@@ -1,28 +1,9 @@
 import PasteBinPage from '../../po/pages/pastebin.page.js';
 
 describe('https://pastebin.com', () => {
-    let page;
     beforeEach(async () => {
         PasteBinPage.open();
-
-        // // add cookies to prevent a pop-up window
-        // await browser.addCookie({name: '_cc_dc', value: '1'});
-        // await browser.addCookie({name: '_cc_id', value: '4a03f9d0af1def51d660c035cd475b46'});
-
-        // // handle cookies pop-up window
-        // await browser.pause(2000);
-        // const cookiesPopupIsDisplayed = await $('div#qc-cmp-ui').isDisplayed();
-        // if (cookiesPopupIsDisplayed) {
-        //     // Find and click the agree button
-        //     await $('button.css-47sehv').click();
-        // }
-
     });
-
-    // it('displays a cookies pop-up window', async () => {
-    //     const cookiesPopup = await $('div#qc-cmp-ui');
-    //     expect(cookiesPopup).toBeDisplayed();
-    // });
 
     it('creates "New Paste" by following the script', async () => {
         await PasteBinPage.newPasteInputField.click();
