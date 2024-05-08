5130f122663810a6cf6a10584c949654cf83720e
Mateusz_Maj <mateusz.maj.krakow@gmail.com>
2024-05-08 09:42:27 +0200

    implement WebdriverIO task 2

--- po/pages/newpaste.page.js
+++ po/pages/newpaste.page.js
@@ -3,9 +3,4 @@ import Page from './Page.js';
 export default new class NewPastePage extends Page {
     get topButtons () {return $('div.top-buttons')}
     get pasteContent () {return $('ol.bash')}
-
-    // open the url with a mock Paste to avoid generating new Paste on every test run
-    async open() {
-        await super.open('https://pastebin.com/saS5vxZZ'); // this url expires on 8.05.2024
-    }
 }
--- test/specs/pastebin.test.js
+++ test/specs/pastebin.test.js
@@ -2,10 +2,35 @@ import pasteBinPage from '../../po/pages/pastebin.page.js';
 import cookiesPopUp from '../../po/elements/cookiesPopUp.element.js';
 import newPastePage from '../../po/pages/newpaste.page.js';
 
-// testing a mock-up Paste page to verify testing logic
-describe('mock-up Paste page', async () => {
+let newPasteUrl;
+
+describe('https://pastebin.com', () => {
     beforeEach(async () => {
-        await newPastePage.open();
+        await pasteBinPage.open();
+
+        await cookiesPopUp.handleCookiesPopUp();
+    });
+
+    it('follows script from Task 2', async () => {
+        await pasteBinPage.newPasteInputField.click();
+        await pasteBinPage.newPasteInputField.setValue('git config --global user.name "New Sheriff in Town" \ngit reset $(git commit-tree HEAD^{tree} -m "Legacy code") \ngit push origin master --force');
+        await pasteBinPage.setSyntaxHighlighting('Bash');
+        await pasteBinPage.setPasteExpiration('1 Day'); // longer expiration than in the task description - due to daily free Pastes limit
+        await pasteBinPage.pasteNameInputField.setValue('how to gain dominance among developers');
+
+        await pasteBinPage.createNewPasteButton.click(); // switch off to avoid exceeding free Pastes limit
+        // wait to load New Paste page
+        await browser.pause(2000);
+        newPasteUrl = await browser.getUrl();
+
+        console.log('url of created Paste: ', newPasteUrl);
+    });
+});
+
+describe('new Paste page', async () => {
+    beforeEach(async () => {
+        await newPastePage.open(newPasteUrl); // make sure createNewPasteButton.click() is on before switching this on
+        // await newPastePage.open('https://pastebin.com/ZSEcaX6K'); // opening a mock-up Paste page; url expires on 8.05.2024
     });
 
     it('has a title matching with the input', async () => {
@@ -18,37 +43,8 @@ describe('mock-up Paste page', async () => {
     });
 
     it('has content matching with the input', async () => {
-        const expectedString = 'git config --global user.name  "New Sheriff in Town"\ngit reset $(git commit-tree HEAD^{tree} -m "Legacy code")\ngit push origin master --force';
+        const expectedString = 'git config --global user.name "New Sheriff in Town" \ngit reset $(git commit-tree HEAD^{tree} -m "Legacy code") \ngit push origin master --force';
         const pasteContent = await newPastePage.pasteContent.getText();
-        expect(pasteContent).toEqual(expect.stringContaining(expectedString));
+        expect(pasteContent).toEqual(expectedString);
     })
 })
-
-// describe('https://pastebin.com', () => {
-//     beforeEach(async () => {
-//         await pasteBinPage.open();
-
-//         await cookiesPopUp.handleCookiesPopUp();
-//     });
-
-//     it('creates "New Paste" by following the script from Task 1', async () => {
-//         await pasteBinPage.newPasteInputField.setValue('Hello from WebDriver');
-//         await pasteBinPage.setPasteExpiration('10 Minutes');
-//         await pasteBinPage.pasteNameInputField.setValue('helloweb');
-//         // await PasteBinPage.createNewPasteButton.click(); // switch off to avoid exceeding free Pastes limit
-//     });
-
-//     it('follows script from Task 2', async () => {
-//         await pasteBinPage.newPasteInputField.click();
-//         await pasteBinPage.newPasteInputField.setValue('git config --global user.name "New Sheriff in Town" \ngit reset $(git commit-tree HEAD^{tree} -m "Legacy code") \ngit push origin master --force');
-//         await pasteBinPage.setSyntaxHighlighting('Bash');
-//         await pasteBinPage.setPasteExpiration('10 Minutes');
-//         await pasteBinPage.pasteNameInputField.setValue('how to gain dominance among developers');
-        
-//         // await PasteBinPage.createNewPasteButton.click(); // switch off to avoid exceeding free Pastes limit
-//         await newPastePage.open(); // switch on to mock-up creating a new paste
-
-//         await expect(browser.getWindowHandle()).toEqual('how to gain dominance among developers');
-
-//     });
-// });
