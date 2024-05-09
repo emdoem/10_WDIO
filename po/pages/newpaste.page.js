128ad5260880004fd23a1c3ec3761809ff2d8121
Mateusz_Maj <mateusz.maj.krakow@gmail.com>
2024-05-09 17:15:34 +0200

    cleanup unnecessary code

--- po/pages/newpaste.page.js
+++
@@ -1,6 +0,0 @@
-import Page from './Page.js';
-
-export default new class NewPastePage extends Page {
-    get topButtons () {return $('div.top-buttons')}
-    get pasteContent () {return $('ol.bash')}
-}
